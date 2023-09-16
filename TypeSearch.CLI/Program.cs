edb0199eed56d3d4dda9a124b68d736ead3ca734
Mårten Åsberg <marten.asberg@outlook.com>
2023-09-16 19:26:56 +0200

    The CLI is somewhat useful

--- TypeSearch.CLI/Program.cs
+++ TypeSearch.CLI/Program.cs
@@ -2,7 +2,7 @@
 using System.Linq;
 using System.Text.Json;
 
-var assemblies = new[] { typeof(string), typeof(Enumerable) }
+var assemblies = new[] { typeof(string) }
 	.Select(t => t.Assembly)
 	.ToArray();
 
@@ -12,10 +12,10 @@ var results = TypeSearch.Domain
 	.Search
 	.Lookup(
 		formulas,
-		new[] { "char", "int" }
+		args.SkipLast(1)
 			.Select(q => TypeSearch.Reflection.Search.TypeFromQuery(assemblies, q))
 			.ToArray(),
-		TypeSearch.Reflection.Search.TypeFromQuery(assemblies, "string"));
+		TypeSearch.Reflection.Search.TypeFromQuery(assemblies, args.Last()));
 
 foreach (var result in results)
 {
